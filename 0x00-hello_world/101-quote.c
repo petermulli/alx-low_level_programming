26865a5e594cb8ccb45eb744ac38e3a65867e738
root <apps.sleek@gmail.com>
2023-09-07 16:17:51 -0700

    header fixed

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
 #!/bin/bash
-include <studio.h>
+#include <studio.h>
 /**
  * main-Entry point
  *
  *return: Always 1 (success)
  */
-int main (void){
+int main(void){
 	write("and that piece of art is useful\"-Dora Korpar, 2015-10-19\n")
 		return (1);
 }
