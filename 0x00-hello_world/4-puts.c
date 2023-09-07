a1119301a4ad2cd3d9e9eef97814ed29ccbb0ddf
root <apps.sleek@gmail.com>
2023-09-07 15:51:09 -0700

    second update for 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,7 +5,7 @@
  *
  *Return: Always 0(Success)
  */
-int main (void){
-	puts ("\"Programming is like builting a multilingual puzzle");
-	return (0);
+int main(void){
+	puts("\"Programming is like builting a multilingual puzzle");
+	return(0);
 }
