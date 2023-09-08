d12ae89748be5c9a1f6bde0a2fecd0170e0e58cf
root <apps.sleek@gmail.com>
2023-09-08 11:25:59 -0700

    Betty compliance updated

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,25 +1,32 @@
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-/**
-*
-*main- Prints positive negative and zero 
-*
-*Return: always (success)
-*
-*/
-int main(void){
-        int n;
 
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        if (n>0){
-                printf("%d is positive\n", n);
-        }else if(n==0){
-                printf("%d is zero\n", n)
-        }else{
-                printf("%d is negative\n", n);
-        }
-        return (0);  
+/**
+ * main - Prints positive, negative, and zero
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else
+	{
+		printf("%d is negative\n", n);
+	}
+
+	return (0);
 }
 
