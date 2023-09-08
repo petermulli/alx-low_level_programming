da49b12d3a0497ee0ce039ffd21bbee0e9856648
root <apps.sleek@gmail.com>
2023-09-08 14:28:41 -0700

    hexadecimal code

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints hexadecimal numbers from 0 to 15.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 15; i++)
+	{
+		printf("%x\n", i);
+	}
+
+	return (0);
+}
+
