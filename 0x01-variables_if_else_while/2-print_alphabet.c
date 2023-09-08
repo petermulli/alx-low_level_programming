a38a54eb9c9eee2132ccb3e4b2ed66ed471c168a
root <apps.sleek@gmail.com>
2023-09-08 15:10:40 -0700

    betty style errors fixed

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -5,16 +5,21 @@
 /**
  * main - Entry point
  *
+ * Description: Prints lowercase alphabets from 'a' to 'z' followed by a newline.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char l = 'a';
+
 	while (l <= 'z')
 	{
 		putchar(l);
 		l++;
 	}
+
+	putchar('\n');
 
 	return (0);
 }
