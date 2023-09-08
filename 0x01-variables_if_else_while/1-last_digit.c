83c4c4fe9f0e93f3477a3d0f58a5f2a4ad571931
root <apps.sleek@gmail.com>
2023-09-08 11:21:20 -0700

    Betty style compliant

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,34 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include<stdio.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int n;
-        int lastDigit;
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        int lastDigit = n%10;
-if (lastDigit > 5)
-{
- printf("Last digit of %d is %d and is greater than 5\n", n, lastDigit)
- } 
-else if (lastDigit = 0)
-{
-Printf ("Last digit of %d is %d and is 0\n", n, lastDigit);
-}
-else 
-{
-Printf ("Last digit of %d is %d and is less than 6 and not 0\n", n, lastDigit)
-}
-return (0);
+	int n;
+	int lsd;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lsd = n % 10;
+
+	if (lsd > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, lsd);
+	}
+	else if (lsd == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, lsd);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lsd);
+	}
+
+	return (0);
 }
 
