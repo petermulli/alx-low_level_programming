c60f6a293bad8cd637e8f47bd60e8c34a49b03c7
root <apps.sleek@gmail.com>
2023-09-08 11:48:46 -0700

    Betty compliance updated

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char u = 'A';
+	char l = 'a';
+
+	while (u <= 'Z')
+	{
+		putchar(u);
+		u++;
+	}
+
+	while (l <= 'z')
+	{
+		putchar(l);
+		l++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
+
