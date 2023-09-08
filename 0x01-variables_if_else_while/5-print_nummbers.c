092672e03b9e6cff40897785bd2ed554259031a6
root <apps.sleek@gmail.com>
2023-09-08 13:12:57 -0700

    5-print_nummbers.c

---
+++ 0x01-variables_if_else_while/5-print_nummbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints numbers from 0 to 9, each on a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
+
