327cdfb18bc1a6aff43a97c466f79580a300d47f
root <apps.sleek@gmail.com>
2023-09-08 15:14:34 -0700

    description fixed

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,8 +3,6 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase, excluding 'e' and 'q', followed by a new line.
- *
  * Return: Always 0 (Success)
  */
 int main(void)
