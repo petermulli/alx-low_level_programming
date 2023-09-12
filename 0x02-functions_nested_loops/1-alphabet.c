9808f3e9df0c920f9195a130de073258803f3e40
root <apps.sleek@gmail.com>
2023-09-12 11:44:15 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *print_alphabets -prints alphabets
+ * Return: return 0
+ */
+void print_alphabet(void)
+{
+char c;
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+putchar('\n');
+}
+
