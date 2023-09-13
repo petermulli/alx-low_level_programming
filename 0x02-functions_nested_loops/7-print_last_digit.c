4f5fd2f85faa339734b2f4db7d3f5be272f0553b
root <apps.sleek@gmail.com>
2023-09-12 17:28:47 -0700

    updated betty description

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,21 +1,23 @@
 #include "main.h"
 
 /**
- * print_last_digit - Print the last digit of a number
+ * print_last_digit - Prints the last digit of a number.
+ * @n: The number whose last digit will be printed.
  *
- * @n: The number whose last digit will be printed
- *
- * Return: The last digit of the number
+ * Return: The last digit of the number.
  */
 int print_last_digit(int n)
-
 {
 	int last;
+
 	last = n % 10;
+
 	if (last < 0)
 	{
-	last = last *-1;
+		last = -last;
 	}
-	 _putchar(last +'0');
+
+	_putchar(last + '0');
 	return (last);
 }
+
