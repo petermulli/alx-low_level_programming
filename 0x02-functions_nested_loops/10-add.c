7aea9a5bd070c91345bcdbf2c51396f5416b4536
root <apps.sleek@gmail.com>
2023-09-12 17:21:36 -0700

    updated betty description

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,5 +1,6 @@
 #include "main.h"
 /*
+ * Description - adds numbers
  * add - Adds two integers and returns the result.
  * @num1: The first integer to be added.
  * @num2: The second integer to be added.
--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,28 +1,28 @@
 #include "main.h"
 #include "stdio.h"
 
-/*
- * print_to_98 - Prints natural numbers from n to 98.
+/**
+ * print_to_98 - Prints numbers from n to 98 inclusively.
  * @n: Starting natural number.
  *
- * Description: Prints numbers from n to 98 inclusively.
  */
 void print_to_98(int n)
 {
 	if (n <= 98)
 	{
-	for (; n <= 98; n++)
-	{
-	if (n == 98)
-	{
-	printf("%d", n);
-	printf("\n");
-	break;
-	}
-	else
-	{
-	printf("%d,", n);
-	}
-	}
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d,", n);
+			}
+		}
 	}
 }
+
