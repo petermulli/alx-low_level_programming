6cdd5854c91f29b336073dc19075ec738061692a
root <apps.sleek@gmail.com>
2023-09-14 05:52:48 -0700

    initial commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks uppercase letter
+ * @y: the number being checked
+ * Return: 1 for uppercase and 0 for anything else
+ */
+int _isupper(int y)
+{
+	if (y >= 65 && y <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
