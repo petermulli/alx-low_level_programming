d6a3a9d79d7e57c1aeaf3a727c5d5ae638522ba9
root <apps.sleek@gmail.com>
2023-09-14 06:50:46 -0700

    multiplier function

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+/**
+ * mul - multiplies two integers
+ * @a: The first integer
+ * @b: The second integer
+ * Return: The product of a and b
+ */
+int mul(int a, int b)
+{
+int res;
+res = a * b;
+return (res);
+}
+
+int main(void)
+{
+return (0);
+}
+
