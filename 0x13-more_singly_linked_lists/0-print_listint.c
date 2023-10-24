61a58543f3d6f792f4eb52cf82b410c6f1365ded
root <apps.sleek@gmail.com>
2023-10-24 06:43:41 -0700

    file updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,22 @@
 #include "lists.h"
 
 /**
- * print_listint - prints linked lists elements
+ * print_listint - prints all the elements of a linked list
  * @h: linked list of type listint_t to print
  *
- * Return: number of nodes involved
+ * Return: number of nodes
  */
 size_t print_listint(const listint_t *h)
 {
- size_t nm = 0;
+ size_t num = 0;
 
  while (h)
  {
  printf("%d\n", h->n);
- nm++;
+ num++;
  h = h->next;
  }
 
- return (nm);
+ return (num);
 }
+
